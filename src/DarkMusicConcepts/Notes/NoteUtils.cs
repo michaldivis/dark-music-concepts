a12c94287d3eac9e4f22dea968f9b30f4f74c20d
Michal Diviš <A408674@dekra.org>
2022-08-04 09:01:23 +0200

    added unit tests for NoteUtils

--- src/DarkMusicConcepts/Notes/NoteUtils.cs
+++ src/DarkMusicConcepts/Notes/NoteUtils.cs
@@ -2,6 +2,12 @@
 
 public static class NoteUtils
 {
+    /// <summary>
+    /// Transpose a pitch
+    /// </summary>
+    /// <param name="pitch">Pitch to transpose</param>
+    /// <param name="interval">Interval to transpose by</param>
+    /// <returns>Transposed pitch</returns>
     public static NotePitch TransposePitch(NotePitch pitch, Interval interval)
     {
         var note = new Note(pitch, Octave.OneLine);
---
+++ tests/DarkMusicConcepts.Tests/Notes/NoteUtilsTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+
+namespace DarkMusicConcepts.Notes.Tests;
+
+public class NoteUtilsTests
+{
+    [Theory]
+    [InlineData(NotePitch.C, 0, NotePitch.C)]
+    [InlineData(NotePitch.C, 1, NotePitch.CSharpOrDFlat)]
+    [InlineData(NotePitch.C, 2, NotePitch.D)]
+    [InlineData(NotePitch.C, 3, NotePitch.DSharpOrEFlat)]
+    [InlineData(NotePitch.C, 4, NotePitch.E)]
+    [InlineData(NotePitch.C, 5, NotePitch.F)]
+    [InlineData(NotePitch.C, 6, NotePitch.FSharpOrGFlat)]
+    [InlineData(NotePitch.C, 7, NotePitch.G)]
+    [InlineData(NotePitch.C, 8, NotePitch.GSharpOrAFlat)]
+    [InlineData(NotePitch.C, 9, NotePitch.A)]
+    [InlineData(NotePitch.C, 10, NotePitch.ASharpOrBFlat)]
+    [InlineData(NotePitch.C, 11, NotePitch.B)]
+    [InlineData(NotePitch.C, 12, NotePitch.C)]
+    public void TransposePitch_ShouldWork(NotePitch notePitch, int intervalDistance, NotePitch expected)
+    {
+        var interval = Interval.CreateIntervalFromDistance(intervalDistance);
+        var transposed = NoteUtils.TransposePitch(notePitch, interval);
+        transposed.Should().Be(expected);
+    }
+
+    [Fact]
+    public void TransposePitch_ShouldNotThrow_ForAllPitchesAndIntervals()
+    {
+        foreach (var notePitch in Enum.GetValues<NotePitch>())
+        {
+            foreach (var interval in Interval.Intervals)
+            {
+                _ = NoteUtils.TransposePitch(notePitch, interval);
+            }
+        }
+    }
+}
