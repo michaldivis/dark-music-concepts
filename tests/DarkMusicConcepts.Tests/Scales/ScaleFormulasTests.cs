3636c194f849a8d0fef03bb7879dc064393839bb
Michal Diviš <michall.divis@gmail.com>
2022-11-30 09:50:39 +0100

    *

--- tests/DarkMusicConcepts.Tests/Scales/ScaleFormulasTests.cs
+++ tests/DarkMusicConcepts.Tests/Scales/ScaleFormulasTests.cs
@@ -8,7 +8,7 @@ public class ScaleFormulasTests
     [Fact]
     public void All_ShouldActuallyContainAllTheScaleFormulas()
     {
-        var found = ReflectionUtils.GetPublicStaticProperties<ScaleFormula>(typeof(ScaleFormula));
+        var found = ReflectionUtils.GetPublicStaticProperties<ScaleFormula>(typeof(ScaleFormulas));
         ScaleFormulas.All.Count.Should().Be(found.Count());
         ScaleFormulas.All.Should().Contain(found);
     }
