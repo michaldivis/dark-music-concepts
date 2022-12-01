457e3f06259755e6a0ce79d229b0466b2090d485
Michal Diviš <A408674@dekra.org>
2022-12-01 08:43:44 +0100

    removed unused code from ChordFormula

--- src/DarkMusicConcepts/Chords/ChordFormula.cs
+++ src/DarkMusicConcepts/Chords/ChordFormula.cs
@@ -21,57 +21,6 @@ public class ChordFormula
     /// </summary>
     public IReadOnlyList<Interval> Intervals { get; }
 
-    public static ChordFormula FunctionForIntervals(IEnumerable<Interval> intervals, int inversion)
-    {
-        if(inversion < 0 || inversion > intervals.Count())
-        {
-            throw new ArgumentOutOfRangeException(nameof(inversion), inversion, "Inversion number is invalid");
-        }
-
-        foreach (var formula in ChordFormulas.All)
-        {
-            if(intervals.Count() != formula.Intervals.Count)
-            {
-                continue;
-            }
-
-            if(IsIntervalSequenceEqual(intervals, formula.Intervals, inversion))
-            {
-                return formula;
-            }
-        }
-
-        //this should never occur
-        throw new ArgumentException("No matching formula found for these intervals and inversion");
-    }
-
-    private static bool IsIntervalSequenceEqual(IEnumerable<Interval> source, IReadOnlyList<Interval> target, int sourceInversion)
-    {
-        static int GetInvertedIndex(int index, int count, int inversion)
-        {
-            var wantedIndex = index + inversion;
-
-            if (wantedIndex > count - 1)
-            {
-                return wantedIndex - count;
-            }
-
-            return wantedIndex;
-        }
-
-        for (int i = 0; i < target.Count; i++)
-        {
-            var index = GetInvertedIndex(i, target.Count, sourceInversion);
-
-            if (source.ElementAt(index) != target[i])
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
     public static IEnumerable<ChordFormula> GetFormulasForScale(Scale scale, Pitch chordRoot)
     {
         var root = Note.Create(chordRoot, Octave.OneLine);
