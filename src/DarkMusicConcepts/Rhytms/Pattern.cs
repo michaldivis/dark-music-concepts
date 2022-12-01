4e79c5c61139db265b788a84c63b264163f33f5d
Michal Diviš <A408674@dekra.org>
2022-12-01 08:43:35 +0100

    added Pattern

---
+++ src/DarkMusicConcepts/Rhytms/Pattern.cs
@@ -0,0 +1,56 @@
+﻿namespace DarkMusicConcepts.Rhytms;
+/// <summary>
+/// Representation of a rhytmical pattern. Each node either is a beat or is empty (true or false)
+/// </summary>
+public class Pattern
+{
+    public bool[] Nodes { get; }
+    public Time NodeDuration { get; }
+    public Time Length { get; }
+
+    private Pattern(bool[] nodes, Time nodeDuration)
+    {
+        Nodes = nodes;
+        NodeDuration = nodeDuration;
+        Length = nodeDuration * nodes.Length;
+    }
+
+    /// <summary>
+    /// Creates a pattern
+    /// </summary>
+    /// <param name="nodes">The nodes</param>
+    /// <param name="nodeDuration">Duration of a node</param>
+    /// <returns>Created pattern</returns>
+    /// <exception cref="ArgumentNullException" />
+    /// <exception cref="ArgumentException" />
+    public static Pattern Create(bool[] nodes, Time nodeDuration)
+    {
+        if(nodes is null)
+        {
+            throw new ArgumentNullException(nameof(nodes));
+        }
+
+        if(nodes.Length == 0)
+        {
+            throw new ArgumentException("Nodes cannot be empty", nameof(nodes));
+        }
+
+        return new Pattern(nodes, nodeDuration);
+    }
+
+    /// <summary>
+    /// Check if a given node index is on a specific pulse.
+    /// <para>
+    /// Example: check if the node with index 7 is on a <see cref="Time.QuarterNoteDotted"/> pulse
+    /// </para>
+    /// </summary>
+    /// <param name="node">Index of the node</param>
+    /// <param name="pulse">Pulse length</param>
+    /// <returns><see langword="true"/> if the node is on the pulse</returns>
+    public bool IsNodeOnPulse(int node, Time pulse)
+    {
+        var nodeTotalLength = NodeDuration * node;
+        var isOnPulse = nodeTotalLength.Ticks % pulse.Ticks == 0;
+        return isOnPulse;
+    }
+}
